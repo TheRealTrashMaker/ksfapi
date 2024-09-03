c0c62754c3383fb052f3f34fa58cc255f6d92ec9
zx <jhawdgawjh@163.com>
2024-09-03 15:42:11 +0800

    update

--- nsetool.py
+++ nsetool.py
@@ -446,4 +446,4 @@ def get_nifty50_data():
 
 if __name__ == '__main__':
     # nes_market(stock_name="BRACEPORT")
-    app.run(host='0.0.0.0', port=5611, debug=False)
+    app.run(host='0.0.0.0', port=5599, debug=False)
